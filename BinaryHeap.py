5058548b63979ba09dc6d3605109dd6f1833a816
DanielTBonn <124016919+DanielTBonn@users.noreply.github.com>
2023-02-01 22:03:40 -0600

    Update BinaryHeap.py
    Includes Max Heap implementation of a Binary Heap as per question 10

--- BinaryHeap.py
+++ BinaryHeap.py
@@ -61,5 +61,67 @@ class MinBinaryHeap:
         return str(self._heap)
 
 
+class MaxBinaryHeap:
+    def __init__(self):
+        self._heap = []
+
+    def _perc_up(self, cur_idx):
+        while (cur_idx - 1) // 2 >= 0:
+            parent_idx = (cur_idx - 1) // 2
+            if self._heap[cur_idx] > self._heap[parent_idx]:
+                self._heap[cur_idx], self._heap[parent_idx] = (
+                    self._heap[parent_idx],
+                    self._heap[cur_idx],
+                )
+            cur_idx = parent_idx
+
+    def _perc_down(self, cur_idx):
+        while 2 * cur_idx + 1 < len(self._heap):
+            max_child_idx = self._get_max_child(cur_idx)
+            if self._heap[cur_idx] < self._heap[max_child_idx]:
+                self._heap[cur_idx], self._heap[max_child_idx] = (
+                    self._heap[max_child_idx],
+                    self._heap[cur_idx],
+                )
+            else:
+                return
+            cur_idx = max_child_idx
+
+    def _get_max_child(self, parent_idx):
+        if 2 * parent_idx + 2 > len(self._heap) - 1:
+            return 2 * parent_idx + 1
+        if self._heap[2 * parent_idx + 1] > self._heap[2 * parent_idx + 2]:
+            return 2 * parent_idx + 1
+        return 2 * parent_idx + 2
+
+    def heapify(self, not_a_heap):
+        self._heap = not_a_heap[:]
+        cur_idx = len(self._heap) // 2 - 1
+        while cur_idx >= 0:
+            self._perc_down(cur_idx)
+            cur_idx = cur_idx - 1
+
+    def get_max(self):
+        return self._heap[0]
+
+    def insert(self, item):
+        self._heap.append(item)
+        self._perc_up(len(self._heap) - 1)
+
+    def delete(self):
+        self._heap[0], self._heap[-1] = self._heap[-1], self._heap[0]
+        result = self._heap.pop()
+        self._perc_down(0)
+        return result
+
+    def is_empty(self):
+        return not bool(self._heap)
+
+    def __len__(self):
+        return len(self._heap)
+
+    def __str__(self):
+        return str(self._heap)
+
 
 
