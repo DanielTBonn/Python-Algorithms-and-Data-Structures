60b4f890127b5f97214b5f4ef8719db099e77fe0
DanielTBonn <124016919+DanielTBonn@users.noreply.github.com>
2023-02-02 06:08:17 -0600

    Solutions to Q6 and Q10 from Chp. 6

--- BinaryHeap.py
+++ BinaryHeap.py
@@ -123,5 +123,60 @@ class MaxBinaryHeap:
     def __str__(self):
         return str(self._heap)
 
+class LimitBinaryHeap(MinBinaryHeap):
+    def __init__(self, limit):
+        super().__init__()
+        self._heap = []
+        self._limit = limit
+
+
+    def _get_max_leaf_idx(self):
+        # Since a binary heap has 2 children for every parent and the largest nodes at the bottom, we only need to search between
+        # half the length of the heap and the end of it
+        leaves = len(self._heap)  // 2
+        max_idx = -1
+
+        for n in range(leaves, len(self._heap)):
+            if self._heap[max_idx] < self._heap[n]:
+                max_idx = n
+
+        return max_idx
+
+    def heapify(self, not_a_heap):
+        self._heap = not_a_heap[:]
+        cur_idx = len(self._heap) // 2 - 1
+        while cur_idx >= 0:
+            self._perc_down(cur_idx)
+            cur_idx = cur_idx - 1
+        
+        reheapify = False
+        while self._limit < len(self._heap):
+            reheapify = True
+            pop_idx = self._get_max_leaf_idx()
+            self.delete(pop_idx)
+        
+        if reheapify:
+            self.heapify(self._heap)
+
+    def insert(self, item):
+        self._heap.append(item)
+        self._perc_up(len(self._heap) - 1)
+
+        
+        if self._limit < len(self._heap):
+            pop_idx = self._get_max_leaf_idx()
+            self.delete(pop_idx)
+
+    def delete(self, pop_idx = 0):
+        result = ''
+        if pop_idx:
+            self._heap[pop_idx], self._heap[-1] = self._heap[-1], self._heap[pop_idx]
+            result = self._heap.pop()
+        else:
+            self._heap[0], self._heap[-1] = self._heap[-1], self._heap[0]
+            result = self._heap.pop(pop_idx)
+        self._perc_down(0)
+        return result
+
 
 
